d348cf3e54000027515d57bec7aae399ac430b3d
Muhammad Haziq <muhammad.haziq@apu.edu.my>
2024-06-30 22:31:41 +0800

    amend plugins: trouble, todo-comments
    Amend a few changes to the plugin

--- lua/haziq/plugins/todo-comments.lua
+++ lua/haziq/plugins/todo-comments.lua
@@ -1,21 +1,21 @@
 return {
-  "folke/todo-comments.nvim", --load plugin URL
-  event = { "BufReadPre", "BufNewFile" }, -- lazy load plugin based on these events 
-  dependencies = { "nvim-lua/plenary.nvim" }, -- mke it planery
-  config = function()
-    local todo_comments = require("todo-comments")
+   "folke/todo-comments.nvim", --load plugin URL
+   event = { "BufReadPre", "BufNewFile" }, -- lazy load plugin based on these events
+   dependencies = { "nvim-lua/plenary.nvim" }, -- mke it planery
+   config = function()
+      local todo_comments = require("todo-comments")
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+      -- set keymaps
+      local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "]t", function() -- to go to the nearest next to-do comment
-      todo_comments.jump_next()
-    end, { desc = "Next todo comment" })
+      keymap.set("n", "]t", function() -- to go to the nearest next to-do comment
+         todo_comments.jump_next()
+      end, { desc = "Next todo comment" })
 
-    keymap.set("n", "[t", function() -- to go to the nearest previous to-do comment
-      todo_comments.jump_prev()
-    end, { desc = "Previous todo comment" })
+      keymap.set("n", "[t", function() -- to go to the nearest previous to-do comment
+         todo_comments.jump_prev()
+      end, { desc = "Previous todo comment" })
 
-    todo_comments.setup()
-  end,
+      todo_comments.setup()
+   end,
 }
