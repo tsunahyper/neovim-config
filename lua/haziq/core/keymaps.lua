6cf3c8f9bcdc885a2f5453e961aa29d6c51414b2
Muhammad Haziq <muhammad.haziq@apu.edu.my>
2024-07-17 10:29:04 +0800

    add: esc terminal keymaps

--- lua/haziq/core/keymaps.lua
+++ lua/haziq/core/keymaps.lua
@@ -3,25 +3,28 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for more conciseness
 
-keymap.set("i", "jk", "<ESC>", { desc =  "Exit insert mode with 'jk' from keyboard"})
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with 'jk' from keyboard" })
+keymap.set("t", "<leader>qa", [[<C-\><C-n>]], { desc = "Exit terminal mode with 'esc' from keyboard" })
+keymap.set("n", "<leader>q", ":term<CR>", { desc = "Open command line on current screen" })
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights"})
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 --keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = "Create shortcut to go to Explore tab"})
+keymap.set("t", "et", "<C-\\><C-n>", { noremap = true, silent = true })
 
 -- Increment & Decrement of numbers for ease of assigned int variables to be scaled
-keymap.set('n', '<leader>+', "<C-a>", { desc = "Increment Number"})
-keymap.set('n', '<leader>-', "<C-x>", { desc = "Decrement Number"})
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment Number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement Number" })
 
 -- Window Management
-keymap.set('n', '<leader>sv', "<C-w>v", { desc = "Split windows vertically"})
-keymap.set('n', '<leader>sh', "<C-w>s", { desc = "Split windows horizontally"})
-keymap.set('n', '<leader>se', "<C-w>=", { desc = "Slit windows to equal size"})
-keymap.set('n', '<leader>sx', "<cmd>close<CR>", { desc = "Close split windows"}) -- Close current split windows that just recently opened
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split windows vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split windows horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Slit windows to equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close split windows" }) -- Close current split windows that just recently opened
 
 -- Tabs Management
-keymap.set('n', '<leader>to', "<cmd>tabnew<CR>", { desc = "Open New Tab"})
-keymap.set('n', '<leader>tx', "<cmd>tabclose<CR>", { desc = "Close Current Tab"})
-keymap.set('n', '<leader>tn', "<cmd>tabn<CR>", { desc = "Go to next tab"})
-keymap.set('n', '<leader>tp', "<cmd>tabp<CR>", { desc = "Go to previous tab"})
-keymap.set('n', '<leader>tf', "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab"}) -- Open current file in a new tab 
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open New Tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close Current Tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) -- Open current file in a new tab
